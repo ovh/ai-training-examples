9c02d3a4ac7623b22294659c67e032094d20c452
Maël LE GAL <mael.le-gal@corp.ovh.com>
2021-07-05 18:04:47 +0200

    [TUTO] Streamlit

---
+++ jobs/streamlit/tuto_simple_app/simple_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title('My first app')
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
