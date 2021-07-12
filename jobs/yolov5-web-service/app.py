f05270fe9273aa0bb2f40b9f5a310ccfc20ad2d6
Eléa P <78957502+eleapttn@users.noreply.github.com>
2021-07-12 15:46:38 +0200

    Update app.py

--- jobs/yolov5-web-service/app.py
+++ jobs/yolov5-web-service/app.py
@@ -35,29 +35,30 @@ def get_prediction_yolov5m(img_bytes):
     results = model_yolov5m(imgs, size=640) 
     return results
 
-@app.route('/', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        # model choice
-        print(f'User selected model : {request.form.get("model_choice")}')
-
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files.get('file')
-        if not file:
-            return
-        
-        img_bytes = file.read()
-        if request.form.get("model_choice") == 'yolov5s':
-            results = get_prediction_yolov5s(img_bytes)
-        if request.form.get("model_choice") == 'yolov5m':
-            results = get_prediction_yolov5m(img_bytes)
-        results.save("static/")  # save the results
-
-        full_filename = os.path.join(app.config['RESULT_FOLDER'], 'image0.jpg')
-        return redirect('static/image0.jpg')
+@app.route('/', methods=['GET'])
+def get():
     return render_template('index.html')
-    #return 'Flask Dockerized'
+
+@app.route('/', methods=['POST'])
+def predict():
+    print(f'User selected model : {request.form.get("model_choice")}')
+
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files.get('file')
+    if not file:
+        return
+    
+    img_bytes = file.read()
+    if request.form.get("model_choice") == 'yolov5s':
+        results = get_prediction_yolov5s(img_bytes)
+    if request.form.get("model_choice") == 'yolov5m':
+        results = get_prediction_yolov5m(img_bytes)
+    results.save("static/")  # save the results
+
+
+    full_filename = os.path.join(app.config['RESULT_FOLDER'], 'image0.jpg')
+    return redirect('static/image0.jpg')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
