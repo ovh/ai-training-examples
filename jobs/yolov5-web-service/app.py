a0cd5822b30bae7eac324dad981e37ed72dea2df
Eléa P <78957502+eleapttn@users.noreply.github.com>
2021-07-12 10:10:51 +0200

    Update app.py

--- jobs/yolov5-web-service/app.py
+++ jobs/yolov5-web-service/app.py
@@ -19,28 +19,26 @@ model_yolov5s.eval()
 model_yolov5m = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True).autoshape() 
 model_yolov5m.eval()
 
-#model_yolov5s = torch.load('yolov5s.pt')
-#print(model_yolov5s)
-#model.load_state_dict(torch.load('yolov5s.pt'))
-#model_yolov5m = torch.load('yolov5m.pt')
-
+# yolov5s
 def get_prediction_yolov5s(img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
     imgs = [img]  # batched list of images
     # Inference
-    results = model_yolov5s(imgs, size=640)  # includes NMS
+    results = model_yolov5s(imgs, size=640) 
     return results
 
+# yolov5m
 def get_prediction_yolov5m(img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
     imgs = [img]  # batched list of images
     # Inference
-    results = model_yolov5m(imgs, size=640)  # includes NMS
+    results = model_yolov5m(imgs, size=640) 
     return results
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
+        # model choice
         print(f'User selected model : {request.form.get("model_choice")}')
 
         if 'file' not in request.files:
@@ -55,10 +53,6 @@ def predict():
         if request.form.get("model_choice") == 'yolov5m':
             results = get_prediction_yolov5m(img_bytes)
         results.save("static/")  # save the results
-        #os.rename("runs/hub/exp/image0.jpg", "static/results.jpg")
-        
-        # delete folder after having renamed it
-        # shutil.rmtree("runs/hub/exp/")
 
         full_filename = os.path.join(app.config['RESULT_FOLDER'], 'image0.jpg')
         return redirect('static/image0.jpg')
