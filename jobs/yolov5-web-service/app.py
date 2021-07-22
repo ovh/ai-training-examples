d885231a5675330e1edd6563844028721439d5ec
Elea Petton <elea.petton@ovhcloud.com>
2021-07-22 13:42:15 +0200

    modif app.py

--- jobs/yolov5-web-service/app.py
+++ jobs/yolov5-web-service/app.py
@@ -4,66 +4,65 @@ import shutil
 import json
 from PIL import Image
 
+import base64
+import cv2
 import torch
 import flask
-from flask import Flask, jsonify, url_for, render_template, request, redirect
+from flask import Flask, jsonify, url_for, render_template, request, redirect, make_response
+import numpy as np
 
 app = Flask(__name__)
 
-RESULT_FOLDER = os.path.join('static')
-app.config['RESULT_FOLDER'] = RESULT_FOLDER
+model_yolov5s = torch.hub.load('ultralytics/yolov5', 'custom', path='models/yolov5s1.pt', force_reload=True)  # default
+model_yolov5m = torch.hub.load('ultralytics/yolov5', 'custom', path='models/yolov5m1.pt', force_reload=True)  # default
 
-model_yolov5s = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).autoshape() 
-model_yolov5s.eval()
-
-model_yolov5m = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True).autoshape() 
-model_yolov5m.eval()
-
-#model_yolov5s = torch.load('yolov5s.pt')
-#print(model_yolov5s)
-#model.load_state_dict(torch.load('yolov5s.pt'))
-#model_yolov5m = torch.load('yolov5m.pt')
 
 def get_prediction_yolov5s(img_bytes):
-    img = Image.open(io.BytesIO(img_bytes))
+    #img = Image.open(io.BytesIO(img_bytes))
+    img = cv2.imdecode(img_bytes, cv2.IMREAD_COLOR)
     imgs = [img]  # batched list of images
     # Inference
-    results = model_yolov5s(imgs, size=640)  # includes NMS
+    results = model_yolov5s(imgs, size=640)  
     return results
 
 def get_prediction_yolov5m(img_bytes):
-    img = Image.open(io.BytesIO(img_bytes))
+    #img = Image.open(io.BytesIO(img_bytes))
+    img = cv2.imdecode(img_bytes, cv2.IMREAD_COLOR)
     imgs = [img]  # batched list of images
     # Inference
-    results = model_yolov5m(imgs, size=640)  # includes NMS
+    results = model_yolov5m(imgs, size=640) 
     return results
 
-@app.route('/', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        print(f'User selected model : {request.form.get("model_choice")}')
 
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files.get('file')
-        if not file:
-            return
-        
-        img_bytes = file.read()
-        if request.form.get("model_choice") == 'yolov5s':
-            results = get_prediction_yolov5s(img_bytes)
-        if request.form.get("model_choice") == 'yolov5m':
-            results = get_prediction_yolov5m(img_bytes)
-        results.save("static/")  # save the results
-        #os.rename("runs/hub/exp/image0.jpg", "static/results.jpg")
-        
-        # delete folder after having renamed it
-        # shutil.rmtree("runs/hub/exp/")
-
-        full_filename = os.path.join(app.config['RESULT_FOLDER'], 'image0.jpg')
-        return redirect('static/image0.jpg')
+@app.route('/', methods=['GET'])
+def get():
     return render_template('index.html')
-    #return 'Flask Dockerized'
+
+
+@app.route('/', methods=['POST'])
+def predict():
+    print(f'User selected model : {request.form.get("model_choice")}')
+
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files.get('file')
+    if not file:
+        return
+    
+    img_bytes = file.read()
+    if request.form.get("model_choice") == 'yolov5s':
+        results = get_prediction_yolov5s(img_bytes)
+    if request.form.get("model_choice") == 'yolov5m':
+        results = get_prediction_yolov5m(img_bytes)
+    
+    results.render()  # updates results.imgs with boxes and labels
+    
+    for img in results.imgs:
+        im_arr = cv2.imencode('.jpg', img)[1]
+        response = make_response(im_arr.tobytes())
+        response.headers['Content-Type'] = 'image/jpeg'
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
