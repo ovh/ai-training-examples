448c8526e11c1014ebb1b4dd964fe047e9f4bdaf
eleapttn <elea.petton@isen-ouest.yncrea.fr>
2022-06-22 15:43:11 +0200

    ai apps gradio + update mnist image classification

---
+++ apps/gradio/app.py
@@ -0,0 +1,47 @@
+# import dependencies
+import gradio as gr
+import tensorflow as tf
+import cv2
+
+# app title
+title = "Welcome on your first sketch recognition app!"
+
+# app description
+head = (
+  "<center>"
+  "<img src='file/mnist-classes.png' width=400>"
+  "The robot was trained to classify numbers (from 0 to 9). To test it, write your number in the space provided."
+  "</center>"
+)
+
+# GitHub repository link
+ref = "Find the whole code [here](ADD github link)."
+
+# image size: 28x28
+img_size = 28
+
+# classes name (from 0 to 9)
+labels = ["zero", "one", "two", "three", "four", "five", "six", "seven", "height", "nine"]
+
+# load model (trained on MNIST dataset)
+model = tf.keras.models.load_model("model/sketch_recognition_numbers_model.h5")
+
+# prediction function for sketch recognition
+def predict(img):
+
+  # image shape: 28x28x1
+  img = cv2.resize(img, (img_size, img_size))
+  img = img.reshape(1, img_size, img_size, 1)
+
+  # model predictions
+  preds = model.predict(img)[0]
+
+  # return the probability for each classe
+  return {label: float(pred) for label, pred in zip(labels, preds)}
+
+# top 3 of classes
+label = gr.outputs.Label(num_top_classes=3)
+
+# open Gradio interface for sketch recognition
+interface = gr.Interface(fn=predict, inputs="sketchpad", outputs=label, title=title, description=head, article=ref)
+interface.launch(server_name="0.0.0.0", server_port=8080)
