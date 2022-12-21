18c881845e5a07c72e1337d595250254cfc6e5ee
Victor <vitcheffvi@cy-tech.fr>
2022-12-21 15:31:42 +0100

    delete django app

--- notebooks/natural-language-processing/chatbot/conda/django_app/chatbot/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    user = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
--- notebooks/natural-language-processing/chatbot/conda/django_app/chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-]
