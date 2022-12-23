49949de590fee6bd5787803f0cbe58bd9e25662f
Victor <vitcheffvi@cy-tech.fr>
2022-12-23 15:28:28 +0100

    change html

--- notebooks/natural-language-processing/chatbot/conda/flask_app/app.py
+++ notebooks/natural-language-processing/chatbot/conda/flask_app/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import render_template
 import jwt
 import os
-import sys
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,9 +11,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     encoded_jwt = jwt.encode({"user": "flask-app"},
-                             str(os.getenv('JWT_SECRET_KEY')), algorithm="HS256")
+                             str(os.getenv('SECRET_KEY')), algorithm="HS256")
     # Create the url back end to have the rasa model train.
-    # print(str(os.getenv('JWT_SECRET_KEY')),file=sys.stderr)
+    # print(str(os.getenv('SECRET_KEY')))
     api_url = os.getenv("API_URL")
     if (api_url == None):
         api_url = 'http://localhost:5005/'
