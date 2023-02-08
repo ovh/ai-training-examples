d969ef657efc8e5c6830a8b9ec9258fa12cef2c2
Mathieu Busquet <62300166+MathieuBsqt@users.noreply.github.com>
2023-02-08 11:36:18 +0100

    Update download_models.py

--- apps/streamlit/speech-to-text/download_models.py
+++ apps/streamlit/speech-to-text/download_models.py
@@ -4,9 +4,9 @@ This script allows you to download the models used by our speech to text app fro
 
 from transformers import pipeline, Wav2Vec2Tokenizer, Wav2Vec2ForCTC, T5Tokenizer, T5ForConditionalGeneration, HubertForCTC, Wav2Vec2Processor
 import pickle
-import torch 
+import torch
 
-        
+
 def load_models():
     # 1 - English Speech to Text Model
     model_name = "facebook/hubert-large-ls960-ft"
@@ -15,7 +15,7 @@ def load_models():
 
     pickle.dump(model, open("/workspace/speech_to_text_app_models/STT_model_hubert-large-ls960-ft.sav", 'wb'))
     pickle.dump(processor, open("/workspace/speech_to_text_app_models/STT_processor_hubert-large-ls960-ft.sav", 'wb'))
-    
+
     # 2 - Summarization Model
     summarizer = pipeline("summarization")
     pickle.dump(summarizer, open("/workspace/speech_to_text_app_models/summarizer.sav", 'wb'))
@@ -25,23 +25,25 @@ def load_models():
     STT_tokenizer = Wav2Vec2Tokenizer.from_pretrained(model_name)
     STT_model = Wav2Vec2ForCTC.from_pretrained(model_name)
 
-    pickle.dump(STT_model, open("/workspace/speech_to_text_app_models/STT_model2_wav2vec2-large-960h-lv60-self.sav", 'wb'))
-    pickle.dump(STT_tokenizer, open("/workspace/speech_to_text_app_models/STT_tokenizer2_wav2vec2-large-960h-lv60-self.sav", 'wb'))
+    pickle.dump(STT_model,
+                open("/workspace/speech_to_text_app_models/STT_model2_wav2vec2-large-960h-lv60-self.sav", 'wb'))
+    pickle.dump(STT_tokenizer,
+                open("/workspace/speech_to_text_app_models/STT_tokenizer2_wav2vec2-large-960h-lv60-self.sav", 'wb'))
 
     # 4 - Auto Punctuation Model
     model_name = "flexudy/t5-small-wav2vec2-grammar-fixer"
     T5_tokenizer = T5Tokenizer.from_pretrained(model_name)
     T5_model = T5ForConditionalGeneration.from_pretrained(model_name)
-    
+
     # (Here T5 Tokenizer can't be saved & loaded with pickle, as pickle serializes this model so it creates a local path in our
     # object, which results in errors when app is not deployed locally anymore. We are going to use torch to save the model
     torch.save(T5_tokenizer, "/workspace/speech_to_text_app_models/T5_tokenizer.sav")
     torch.save(T5_model, "/workspace/speech_to_text_app_models/T5_model.sav")
-    #pickle.dump(T5_tokenizer, open("/workspace/speech_to_text_app_models/spiece.model", 'wb'))
-    #pickle.dump(T5_model, open("/workspace/speech_to_text_app_models/T5_model.sav", 'wb'))
-    
+    # pickle.dump(T5_tokenizer, open("/workspace/speech_to_text_app_models/spiece.model", 'wb'))
+    # pickle.dump(T5_model, open("/workspace/speech_to_text_app_models/T5_model.sav", 'wb'))
+
     # 5 - Diarization model - Can't be saved anymore since pyannote.audio v2
-   
+
 
 if __name__ == '__main__':
     load_models()
