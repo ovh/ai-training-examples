a76f4436b23c8a736f579de2b45ad60586c91e73
Eléa Petton <elea.petton@isen-ouest.yncrea.fr>
2023-07-20 11:26:33 +0200

    📈 add tensorboard (#86)
    * 📈 add tensorboard
    
    * fix: 🐛 remove extra parenthesis
    
    * fix: 🐛 compile pb
    
    * feat: ✨ use Python base image and trained model
    
    * fix: 🐛 local path for data
    
    ---------
    
    Co-authored-by: Elea Petton <elea.petton@ovhcloud.com>
    Co-authored-by: philippart-s <wildagsx@gmail.com>

--- jobs/audio/audio-classification/train-audio-classification.py
+++ jobs/audio/audio-classification/train-audio-classification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import datetime
 
 # preprocessing
 from sklearn.preprocessing import LabelEncoder
@@ -7,9 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 # model
-from keras.models import load_model
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
 
 ########################################################################################################################################################
 # The goal of this script is to train a pre-construct model to recognize marine mammal sound.                                                          #
@@ -17,7 +16,7 @@ from tensorflow.keras.models import Sequential
 # more details : https://github.com/ovh/ai-training-examples/blob/main/notebooks/audio/audio-classification/notebook-marine-sound-classification.ipynb #
 # You must mount 2 volumes for the data and the model (the same used for the Notebook for example 😉) :                                                #
 #   - /workspace/saved_model where the model is stored                                                                                                 #
-#   - /workspace/data where store the data for the training                                                                                            #
+#   - /workspace/data/ where store the data for the training                                                                                            #
 ########################################################################################################################################################
 
 
@@ -43,15 +42,35 @@ print("X:", X)
 # ⚗️ Create training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2)
 
-# 🧠 Load the pre-trained model 
-model = load_model('/workspace/saved_model/my_model')
+# 🧠 Define model architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(512, activation='relu', input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(45, activation='softmax'),
+])
+
 print(model.summary())
 
 # 💪 Train the model with data
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = 'accuracy')
-batch_size = 128
-model.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 100, batch_size = batch_size)
 
-# 💿 Save the model for futur usages
+# 📈 Add the TensorBoard callback (optional)
+print('Model tracking')
+log_dir = "/workspace/saved_model/runs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+model.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = 100, batch_size = 128, callbacks = [tensorboard_callback])
+
+# 💿 Save the model for future usages
 model.save('/workspace/saved_model/my_model2')
 print('End of training')
